f63769180abfee6d1cdc8e552d22dd5a1dd7f1e2
Victor Mesquini <mesquini@live.com>
2020-01-22 11:29:27 -0300

    ajuste no logo
    ajuste no logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import './Header.css'
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo.png'
 import camera from '../assets/camera.svg'
 
 export default function Header (){
@@ -9,7 +9,7 @@ export default function Header (){
         <header id="main-header">
             <div className="header-content">
                 <Link to="/">
-                    <img src={logo} alt="InstaRocket"></img>
+                    <img style={{width: 50}} src={logo} alt="InstaRocket"></img>
                 </Link>
                 <Link to="/new">
                     <img src={camera} alt="Enviar publicação"></img>
